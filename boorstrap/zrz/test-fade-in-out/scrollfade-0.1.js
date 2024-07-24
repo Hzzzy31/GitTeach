e9a989265a08af4742ae2eecb67fe1171027d129
ruizhe <230600746@qq.com>
2024-07-24 16:24:02 +0800

    注释一些代码

--- boorstrap/zrz/test-fade-in-out/scrollfade-0.1.js
+++ boorstrap/zrz/test-fade-in-out/scrollfade-0.1.js
@@ -13,15 +13,15 @@ function scrollFade() {
 		var fadeInPoint = window.innerHeight - elementFourth;
 		var fadeOutPoint = -(rect.height / 2);
 
-		if (rect.top <= fadeInPoint) {
+		if (rect.top <= fadeInPoint) { 				/* 如果到顶部的距离少了就出现 */
 			element.classList.add('scrollFade--visible');
 			element.classList.add('scrollFade--animate');
 			element.classList.remove('scrollFade--hidden');
-		} else {
+		} else { 									/* 如果到顶部的距离多了就消失 */
 			element.classList.remove('scrollFade--visible');
 			element.classList.add('scrollFade--hidden');
 		}
-
+		// 控制已经出现的元素如果脱离视图时的消失
 		if (rect.top <= fadeOutPoint) {
 			element.classList.remove('scrollFade--visible');
 			element.classList.add('scrollFade--hidden');
