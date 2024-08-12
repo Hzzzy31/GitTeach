b9e5210795a90aaeeb98ff47e58071f29a7b298c
ruizhe <230600746@qq.com>
2024-08-12 14:31:25 +0800

    文本框

---
+++ boorstrap/zrz/layout/1.js
@@ -0,0 +1,17 @@
+function resetAnimation() {
+  var spans = document.querySelectorAll('span');
+  spans.forEach(span => {
+    span.style.animation = 'none';
+    span.offsetHeight;
+    span.style.animation = null;
+  })
+}
+/* 画框 */
+function init(e) {
+  if (!e.target.closest('.item')) return;
+  let hero = document.querySelector('div[data-pos="0"]');
+  let target = e.target.parentElement;
+  [target.dataset.pos, hero.dataset.pos] = [hero.dataset.pos, target.dataset.pos];
+}
+
+window.addEventListener('click', init, false);
